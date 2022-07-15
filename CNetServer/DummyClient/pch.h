727e2d1e08d393c6109abd287800a45b8fd614c2
BerryBless <shsuzakfn@gmail.com>
2022-07-15 13:31:02 +0900

    INIT setting

---
+++ CNetServer/DummyClient/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma comment(lib, "Debug\\ServerCore.lib")
+#else
+#pragma comment(lib, "Release\\ServerCore.lib")
+#endif
+
+#include "CorePch.h"
---
+++ CNetServer/EchoServer/main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main() {
+	HelloWorld();
+	return 0;
+}
