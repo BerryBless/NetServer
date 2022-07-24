31cb6115b85bbf373d2aa34870d3cb70708cb091
BerryBless <shsuzakfn@gmail.com>
2022-07-25 08:07:37 +0900

    CNetServer - Send Thread stability Test

---
+++ Backup/CNetServer - Send Thread stability Test/DummyClient/CEchoClient.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "CLanClient.h"
+#define df_PACKET_CODE 0x73
+#define df_SENDOVER 100
+class CEchoClient : public CLanClient {
+public:
+	struct Protocol {
+		BYTE _code; // 0x73
+		DWORD _cnt;
+		DWORD _sendTime;
+		BOOL _recved;
+	};
+
+public:
+	CEchoClient();
+	~CEchoClient();
+	void Test();
+private:
+	virtual void OnEnterJoinServer();
+	virtual void OnLeaveServer();
+	virtual void OnRecv(CPacket *);
+	virtual void OnSend(int sendsize);
+	virtual void OnError(int errorcode, const WCHAR *);
+
+	void sendTest();
+	void recvTest();
+	void recvCheck(CPacket *);
+
+private:
+	DWORD _login = 0;
+	DWORD _cnt = 0;
+
+	bool _wait;
+
+	DWORD _totalRTT =0 ;
+	DWORD _countRTT =0;
+
+	Protocol _testPacket[df_SENDOVER];
+};
+
---
+++ Backup/CNetServer - Send Thread stability Test/ServerCore/CorePch.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma comment(lib, "ws2_32")
+#pragma comment (lib, "winmm")
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <locale.h>
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+#include <iostream>
+#include <timeapi.h>
+#include "CRingBuffer.h"
+#include "CLogger.h"
+#include "CObjectPool.hpp"
+#include "CCrashDump.h"
+#include "Stack.hpp"
+#include "Queue.hpp"
+#include "HardWareMoniter.h"
+#include "ProcessMoniter.h"
+#include "SerializingBuffer.h"
+#include "CPacket.h"
+using namespace std;
+
+
+
+#define CRASH() do{\
+					CLogger::_Log(dfLOG_LEVEL_ERROR, L"///////CRASH : FILE[%s] Line[%d]",__FILEW__,__LINE__);\
+					CCrashDump::Crash();\
+				}while(0)
+
+
+
+
+void HelloWorld();
